c9400ee8a1a50174cb94b0a9fb30ed9edcc947e3
Dany Hakim <dany_hakim@yahoo.com>
2022-11-08 14:27:16 +0700

    Initial Commit

---
+++ SALT Technical Test App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SALT Technical Test App
+//
+//  Created by Dany Elita on 08/11/22.
+//  Copyright © 2022 Dany Hakim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
